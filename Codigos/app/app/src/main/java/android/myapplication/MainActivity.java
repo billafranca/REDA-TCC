f157bbd3eb0c7de920c15a36480ccba548592cd7
billafranca <bragap379@gmail.com>
2026-02-25 12:47:35 -0300

    desenho tela login

--- Codigos/app/app/src/main/java/android/myapplication/MainActivity.java
+++ Codigos/app/app/src/main/java/android/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package android.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+TextView txtTitle;
+EditText EdtText;
 
+Button btnGoogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +25,16 @@ public class MainActivity extends AppCompatActivity {
         initComponents();
 
 
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void initComponents() {
+        txtTitle = findViewById(R.id.txt_title);
+        EdtText = findViewById(R.id.edt_email);
+        btnGoogle = findViewById(R.id.btn_google);
     }
 }
