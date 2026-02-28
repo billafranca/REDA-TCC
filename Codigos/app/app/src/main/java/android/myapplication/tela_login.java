fc06357ddc7e422cf143505eee076e8335f6c303
Lucas49l <lucasguimaraes1649@gmail.com>
2026-02-28 14:31:36 -0300

    Update codigo android

--- Codigos/app/app/src/main/java/android/myapplication/tela_login.java
+++ Codigos/app/app/src/main/java/android/myapplication/tela_login.java
@@ -1,10 +1,6 @@
 package android.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,35 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-EditText EdtText;
-Button login;
+public class tela_login extends AppCompatActivity {
 
-Button btnGoogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        initComponents();
-
-        //Evento para trocar de página da main para a tela de login
-        login.setOnClickListener(V ->{
-            Intent intent = new Intent(MainActivity.this, tela_login.class);
-            startActivity(intent);
-        });
-
+        setContentView(R.layout.activity_tela_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    private void initComponents() {
-        EdtText   = findViewById(R.id.edt_email);
-        btnGoogle = findViewById(R.id.btn_google);
-        login     = findViewById(R.id.btn_Login);
     }
 }
